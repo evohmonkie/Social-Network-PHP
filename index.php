706f250c61399e44f5b2690380bae8eb8b83803c
tlee <evohmonkie@gmail.com>
2016-08-13 21:29:33 -0500

    fixed merge conflict

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
-	<title></title>
-=======
 	<title>Social Network Tutorial</title>
 	<!-- CSS -->
 	<link rel="stylesheet" href="css/main.css">
->>>>>>> web-fundamentals
 </head>
 <body>
 
