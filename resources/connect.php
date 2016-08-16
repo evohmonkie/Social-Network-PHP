1fcc83c2588443f4f07c376880f26613667852da
Teng Lee <evohmonkie@gmail.com>
2016-08-16 22:22:15 +0000

    reconfigured files to work with c9

--- resources/connect.php
+++ resources/connect.php
@@ -1,11 +1,17 @@
 <?php
+// $database = "social_network";
+// $servername = "localhost";
+// $username = "root";
+// $password = "monkies";
 $database = "social_network";
-$servername = "localhost";
-$username = "root";
-$password = "monkies";
+$servername = getenv("REMOTE_ADDR");
+$port = 3306;
+$username = "evohmonkie";
+
 
 // Connect to database
-$connect = mysqli_connect($servername, $username, $password, $database);
+// $connect = mysqli_connect($servername, $username, $password, $database);
+$connect = mysqli_connect($servername, $username, "", $database, $port);
 
 // Check databse connection
 if (!$connect) {
